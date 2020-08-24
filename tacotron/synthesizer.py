d8d9a661bab653629fee7a3785d44598ed905b3c
wangjiaqi12807 <wangjiaqi12807@autohome.com.cn>
2020-08-24 17:07:29 +0800

    edit mel feature path

--- tacotron/synthesizer.py
+++ tacotron/synthesizer.py
@@ -14,72 +14,62 @@ from infolog import log
 
 
 class Synthesizer:
-    def load(self, checkpoint_path, hparams, gta=False, model_name='Tacotron'):
-        log('Constructing model: %s' % model_name)
-        inputs = tf.placeholder(tf.int32, [1, None], 'inputs')
-        input_lengths = tf.placeholder(tf.int32, [1], 'input_lengths')
-        targets = tf.placeholder(tf.float32, [1, None, hparams.num_mels], 'mel_targets')
-        with tf.variable_scope('model') as scope:
-            self.model = create_model(model_name, hparams)
-            if gta:
-                self.model.initialize(inputs, input_lengths, targets, gta=gta)
-            else:        
-                self.model.initialize(inputs, input_lengths)
-            self.mel_outputs = self.model.mel_outputs
-            self.alignment = self.model.alignments[0]
+	def load(self, checkpoint_path, hparams, gta=False, model_name='Tacotron'):
+		log('Constructing model: %s' % model_name)
+		inputs = tf.placeholder(tf.int32, [1, None], 'inputs')
+		input_lengths = tf.placeholder(tf.int32, [1], 'input_lengths')
+		targets = tf.placeholder(tf.float32, [1, None, hparams.num_mels], 'mel_targets')
+		with tf.variable_scope('model') as scope:
+			self.model = create_model(model_name, hparams)
+			if gta:
+				self.model.initialize(inputs, input_lengths, targets, gta=gta)
+			else:
+				self.model.initialize(inputs, input_lengths)
+			self.mel_outputs = self.model.mel_outputs
+			self.alignment = self.model.alignments[0]
 
-        self.gta = gta
-        self._hparams = hparams
+		self.gta = gta
+		self._hparams = hparams
 
-        log('Loading checkpoint: %s' % checkpoint_path)
-        self.session = tf.Session()
-        self.session.run(tf.global_variables_initializer())
-        saver = tf.train.Saver()
-        saver.restore(self.session, checkpoint_path)
-#mel_filename = synth.synthesize(text, i+1, eval_dir, log_dir, None)
+		log('Loading checkpoint: %s' % checkpoint_path)
+		self.session = tf.Session()
+		self.session.run(tf.global_variables_initializer())
+		saver = tf.train.Saver()
+		saver.restore(self.session, checkpoint_path)
 
-    def synthesize(self, text, index, out_dir, log_dir, mel_filename):
-        hparams = self._hparams
-        cleaner_names = [x.strip() for x in hparams.cleaners.split(',')]
-        seq = text_to_sequence(text, cleaner_names)
-        print(text.split(' '))
-        print(seq)
-        feed_dict = {
-            self.model.inputs: [np.asarray(seq, dtype=np.int32)],
-            self.model.input_lengths: np.asarray([len(seq)], dtype=np.int32),
-        }
+	# mel_filename = synth.synthesize(text, i+1, eval_dir, log_dir, None)
 
-        if self.gta:
-            print(mel_filename)
-            #feed_dict[self.model.mel_targets] = np.load(mel_filename).reshape(1, -1, 80)
-            feed_dict[self.model.mel_targets] = np.load(mel_filename).reshape(1, -1, 40)
-        if self.gta or not hparams.predict_linear:
-            mels, alignment = self.session.run([self.mel_outputs, self.alignment], feed_dict=feed_dict)
+	def synthesize(self, text, index, out_dir, log_dir, mel_filename):
+		hparams = self._hparams
+		cleaner_names = [x.strip() for x in hparams.cleaners.split(',')]
+		seq = text_to_sequence(text, cleaner_names)
+		print(text)
+		print(seq)
+		feed_dict = {
+			self.model.inputs: [np.asarray(seq, dtype=np.int32)],
+			self.model.input_lengths: np.asarray([len(seq)], dtype=np.int32),
+		}
 
-        else:
-            linear, mels, alignment = self.session.run([self.linear_outputs, self.mel_outputs, self.alignment], feed_dict=feed_dict)
-            linear = linear.reshape(-1, hparams.num_freq)
+		if self.gta:
+			# feed_dict[self.model.mel_targets] = np.load(mel_filename).reshape(1, -1, 80)
+			feed_dict[self.model.mel_targets] = np.load(mel_filename).reshape(1, -1, 40)
+		if self.gta or not hparams.predict_linear:
+			mels, alignment = self.session.run([self.mel_outputs, self.alignment], feed_dict=feed_dict)
 
-        mels = mels.reshape(-1, hparams.num_mels) #Thanks to @imdatsolak for pointing this out
+		else:
+			linear, mels, alignment = self.session.run([self.linear_outputs, self.mel_outputs, self.alignment],
+													   feed_dict=feed_dict)
+			linear = linear.reshape(-1, hparams.num_freq)
 
-        #convert checkpoint to frozen model
-        minimal_graph = tf.graph_util.convert_variables_to_constants(self.session, self.session.graph_def, ["model/inference/add"])
-        tf.train.write_graph(minimal_graph, '.', 'inference_model.pb', as_text=False)
+		mels = mels.reshape(-1, hparams.num_mels)  # Thanks to @imdatsolak for pointing this out
 
-        npy_data = mels.reshape((-1,))
-        #print(mels)
-        print("==============================================")
-        #print(npy_data)
-        text = text.replace (" ", "_")
-        text = text.replace ("?","")
-        filename=text+'.f32'
-        print(mel_filename)
-        #filename=mel_filename.split('/')
-        fname = filename[-1][4:-3]+'f32'
-        f32name=os.path.join(log_dir,'wavs/wav-{}-me.f32'.format(index))#by jiang
-        #f32name='/data/jiangpeipei/begeekmyfriend/LPCTron/Tacotron-2'+'/feaf32/'+fname
-        print(f32name)#by jiang
-        #npy_data.tofile(filename)#by jiang
-        npy_data.tofile(f32name)#by jiang
-        npy_data.tofile('/data1/jiangpeipei/begeekmyfriend/LPCTron/feature32/feature-{}.f32'.format(index))
-        return
+		# convert checkpoint to frozen model
+		minimal_graph = tf.graph_util.convert_variables_to_constants(self.session, self.session.graph_def,
+																	 ["model/inference/add"])
+		tf.train.write_graph(minimal_graph, '.', 'inference_model.pb', as_text=False)
+
+		npy_data = mels.reshape((-1,))
+
+		f32name = os.path.join(out_dir, 'mels/feature-{}.f32'.format(index))  # by jiang
+		npy_data.tofile(f32name)  # by jiang
+		return
