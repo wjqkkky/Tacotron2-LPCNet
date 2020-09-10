eb1505ab1d02cfb5341b714ef1dccaef946fc9ca
wangjiaqi12807 <wangjiaqi12807@autohome.com.cn>
2020-09-10 12:26:45 +0800

    fk

--- front_end_main.py
+++ front_end_main.py
@@ -79,6 +79,7 @@ def add_Rhy_self(chinese: str):
 
 def chinese2py(chinese):
 	chinese = chinese.replace('、', ',').replace('：', ',').replace(';', ',').replace('；', ',')
+	r4 = "\\【.*?】+|\\《.*?》+|\\#.*?#+|[/_$&%^*()<>+""'@|~{}#]+|[——\\\=、：\\-\"“”‘’‘'￥……（）\\[\\] 《》【】]"
 	chinese = re.sub(r4, '', chinese)
 	chinese = add_Rhy_self(chinese)
 	chinese = chinese.upper()
